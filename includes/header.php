186333edc8937dab45a1fba78472f3be29c34c91
nadia-rais <nadia.rais@laplateforme.io>
2020-06-22 09:18:09 +0200

    fichiers de base
    création des pages forum avec structure globale

---
+++ includes/header.php
@@ -0,0 +1,33 @@
+<?php 
+if (isset($_POST["deco"])) {
+    session_unset();
+    session_destroy();
+    header('Location:index.php');
+}
+?>
+
+<header>
+  <nav>
+    <img id="logo1" src="https://i.ibb.co/T0XWW6V/logo2-wow.png" alt="logo2-wow">
+    <a id="header-title" href="index.php">THE WOW'S CLAN</a>
+    <ul id="nav__links">
+      <li><a href="categorie.php">CATÉGORIES</a></li>
+      <li><a href="topic.php">TOPICS</a></li>
+
+      <?php 
+        if (isset($_SESSION["login"])):
+      ?>    
+      <li><a href="profil.php">PROFIL</a></li>
+      <li><form action="index.php" method="post">
+            <input id="deco" name="deco" value="DECONNEXION" type="submit"/>
+          </form>
+      </li>
+
+      <?php
+        else :
+      ?>
+      <li><a href="connexion.php"> LOGIN &nbsp;</a><i class="far fa-user"></i></li>
+      <?php endif;?>
+    </ul>
+  </nav>
+</header>
