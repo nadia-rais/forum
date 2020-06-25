ce4d948190cacbedc9031186165bd1b8179dd54b
gregory-lopez <62139827+gregory-lopez@users.noreply.github.com>
2020-06-25 10:48:14 +0200

    ajout d'un header temporaire

--- admin.php
+++ admin.php
@@ -13,7 +13,61 @@ session_start();
 </head>
 <body>
     <header>
+    <head>
+        <title>Connexion</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/discussion.css">
+        <link href="https://fonts.googleapis.com/css2?family=Chivo&family=Noto+Sans+JP&display=swap" rel="stylesheet">
+    </head>
 
+    <body>
+        
+        <header>
+            <section>
+
+                <?php
+
+                if(isset($_SESSION['login']) && ($_SESSION['login']!='admin')) {
+    
+                    echo "
+
+                <nav>
+                <ul>
+                    <li><a href='index.php'>Accueil</a></li>
+                    <li><a href='message.php'>Chat</a></li>
+                    <li><a href='profil.php'>Profil</a></li>
+                    <li><a href='logout.php'>Déconnexion</a></li>
+                </ul>
+                </nav>
+
+                 ";}
+                 elseif(isset($_SESSION['login']) && ($_SESSION['login']=='admin')) {
+                    
+                    echo "
+
+                    <nav>
+                    <ul>
+                        <li><a href='index.php'>Accueil</a></li>
+                        <li><a href='message.php'>Chat</a></li>
+                        <li><a href='profil.php'>Profil</a></li>
+                        <li><a href='admin.php'>Admin</a></li>
+                        <li><a href='logout.php'>Déconnexion</a></li>
+                    </ul>
+                    </nav>
+    
+                     ";}
+                else{
+                    echo "
+
+                <nav>
+                <ul>
+                    <li><a href='index.php'>Accueil</a></li>
+                    <li><a href='inscription.php'>Inscription</a></li>
+                    <li><a href='connexion.php'>Connexion</a></li>
+                </ul>
+                </nav>
+
+                 ";} ?>
     </header>
     <main>
       <section class="info2">
