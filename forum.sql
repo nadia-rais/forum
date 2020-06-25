7167faabaed92548c6616fa85c4462828618b597
gregory-lopez <62139827+gregory-lopez@users.noreply.github.com>
2020-06-25 10:11:43 +0200

    Supression de la table CONVERSATION / Ajout de la table message SANS le champ "id_topic (int)" (Ré ajoutez dans la finalité)

--- forum.sql
+++ forum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  ven. 19 juin 2020 à 10:19
+-- Généré le :  jeu. 25 juin 2020 à 08:07
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -67,17 +67,32 @@ CREATE TABLE IF NOT EXISTS `liker` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `message`
+-- Structure de la table `messages`
 --
 
-DROP TABLE IF EXISTS `message`;
-CREATE TABLE IF NOT EXISTS `message` (
+DROP TABLE IF EXISTS `messages`;
+CREATE TABLE IF NOT EXISTS `messages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `msg` varchar(255) NOT NULL,
+  `message` varchar(140) NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
-  `id_conversation` int(11) NOT NULL,
+  `date` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `messages`
+--
+
+INSERT INTO `messages` (`id`, `message`, `id_utilisateur`, `date`) VALUES
+(26, 'greg', 5, '2020-06-25'),
+(25, 'z', 6, '2020-06-25'),
+(24, 'zz', 5, '2020-06-25'),
+(23, '', 5, '2020-06-25'),
+(22, 'g', 5, '2020-06-25'),
+(21, '3\r\n', 5, '2020-06-25'),
+(20, 'Test ', 5, '2020-06-23'),
+(19, 'Salut', 5, '2020-06-23'),
+(18, 'Salutations voyageurs\r\n', 5, '2020-06-23');
 
 -- --------------------------------------------------------
 
@@ -87,10 +102,19 @@ CREATE TABLE IF NOT EXISTS `message` (
 
 DROP TABLE IF EXISTS `signaler`;
 CREATE TABLE IF NOT EXISTS `signaler` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `signalement` varchar(255) NOT NULL,
   `id_utilisateur` int(11) NOT NULL,
-  `id_message` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `id_message` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `signaler`
+--
+
+INSERT INTO `signaler` (`id`, `signalement`, `id_utilisateur`, `id_message`) VALUES
+(11, 'grezg', 5, 26);
 
 -- --------------------------------------------------------
 
@@ -120,7 +144,16 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `email` varchar(255) NOT NULL,
   `id_droits` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `login`, `password`, `email`, `id_droits`) VALUES
+(6, 'rodolphe', '123', 'gregpsn365@hotmail.com', 1),
+(5, 'greg', '123', 'gregpsn365@hotmail.com', 1),
+(7, 'nadia', '123', 'gregpsn365@hotmail', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
